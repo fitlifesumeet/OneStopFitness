bf2850554fd3a3a03a6ba4d44c0ee8f6312d0664
fitlifesumeet <fitlifesumeet@gmail.com>
2025-08-26 16:49:12 -0400

    Create next.config.js

---
+++ apps/web/next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'standalone', // ensures Vercel knows how to handle the build
+};
+
+module.exports = nextConfig;
